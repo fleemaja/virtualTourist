06366c4e4f387481c3cd84192e107745502dbb18
Drew Fleeman <drew.fleeman@gmail.com>
2017-07-06 15:37:27 -0500

    add storyboard scaffold

---
+++ virtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoAlbumViewController.swift
+//  virtualTourist
+//
+//  Created by Drew Fleeman on 7/6/17.
+//  Copyright © 2017 drew. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ virtualTourist/TravelLocationsMapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TravelLocationsMapViewController.swift
+//  virtualTourist
+//
+//  Created by Drew Fleeman on 7/6/17.
+//  Copyright © 2017 drew. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsMapViewController: UIViewController {
+
+
+}
+
